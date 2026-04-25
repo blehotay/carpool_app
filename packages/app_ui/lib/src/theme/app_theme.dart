779784ac556ce9a98f71f9a0218b97c67ed546da
blehotay <ben@theappdistillery.com>
2026-04-25 12:34:43 -0400

    docs: complete app_ui package documentation
    - Document AppSpacing with all spacing constants
    - Document AppTheme class
    - Document AppTextStyles with all text style constants
    - All app_ui public APIs now fully documented
    - All tests passing (14/14)

--- packages/app_ui/lib/src/theme/app_theme.dart
+++ packages/app_ui/lib/src/theme/app_theme.dart
@@ -2,7 +2,9 @@ import 'package:app_ui/src/colors/app_colors.dart';
 import 'package:app_ui/src/typography/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
+/// Application theme configuration.
 abstract class AppTheme {
+  /// Light theme using Material 3 design.
   static ThemeData get light {
     return ThemeData(
       useMaterial3: true,
